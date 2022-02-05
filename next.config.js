b00e0b9e663c4c6fcc4148707b2f42e055f95317
siemen <siemensubbaiah1@gmail.com>
2022-02-05 13:13:08 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,22 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/order/:id',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
